4b44a88f9c9151517400baddaf8bb3a805c65b78
ErikNils <erik1.nilsson2@gmail.com>
2021-12-13 15:06:47 +0100

    Made individual test classes

---
+++ src/test/java/murraco/HeapsortTest.java
@@ -0,0 +1,105 @@
+package murraco;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.generator.NullAllowed;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+//import com.sun.istack.internal.Nullable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnitQuickcheck.class)
+
+public class HeapsortTest {
+
+    @Property
+    public void shouldSortPositiveAndNegative(Integer[] data) {
+        //System.out.println(Arrays.toString(data));
+
+        Integer[] copy = data.clone();
+        Heapsort.heapSort(copy);
+        Arrays.sort(data);
+
+        assertArrayEquals(copy, data);
+    }
+
+
+    @Property
+    public void shouldSortStrings(String[] data) {
+        //System.out.println(Arrays.toString(data));
+
+        String[] copy = data.clone();
+        Heapsort.heapSort(copy);
+        Arrays.sort(data);
+
+        assertArrayEquals(copy, data);
+    }
+
+    @Property
+    public void shouldSortListWithOneValue(Integer n) {
+        Integer[] data = {n};
+        //System.out.println(Arrays.toString(data));
+        Integer[] copy = data.clone();
+        Heapsort.heapSort(copy);
+        Arrays.sort(data);
+
+        assertArrayEquals(copy, data);
+    }
+
+    @Property
+    public void shouldSortListWithDoubleValues(Double[] data) {
+        //System.out.println(Arrays.toString(data));
+        Double[] copy = data.clone();
+        Heapsort.heapSort(copy);
+        Arrays.sort(data);
+
+        assertArrayEquals(copy, data);
+    }
+
+
+    // Will try to make a quickCheck generator that does this instead.
+    @Property
+    public void shouldSortListWithRepeatedElements(Integer[] data) {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = data[0];
+        }
+        //System.out.println(Arrays.toString(data));
+        Integer[] copy = data.clone();
+        Heapsort.heapSort(copy);
+        Arrays.sort(data);
+
+        assertArrayEquals(copy, data);
+    }
+
+    @Property
+    public void shouldSortListWithNulls(@NullAllowed(probability = 0.1f) Integer[] data) {
+        //System.out.println(Arrays.toString(data));
+        try {
+            Integer[] copy = data.clone();
+            Heapsort.heapSort(copy);
+            Arrays.sort(data);
+
+            assertArrayEquals(copy, data);
+        } catch (NullPointerException e) {
+            assertNull(data);
+        }
+    }
+
+    @Test(expected = NegativeArraySizeException.class)
+    public void shouldThrowNegativeArrayException() {
+        Integer[] data = new Integer[-2];
+        Heapsort.heapSort(data);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerException() {
+        Integer[] data = new Integer[2];
+        Heapsort.heapSort(data);
+    }
+}
